5cae11d600bc0973fec6d4a6737f9ebb21e950c0
FadhilAlif <92931140+FadhilAlif@users.noreply.github.com>
2023-10-12 22:54:42 +0700

    delete: doubled file

--- 23_Basic Model OpenAI dan Prompt Engginer di React/Praktikum/My_OpenAI/src/components/Button/index.jsx
+++
@@ -1,9 +0,0 @@
-const Button = ({ text, onClick, className, id }) => {
-  return (
-    <button className={className} id={id} onClick={onClick}>
-      {text}
-    </button>
-  );
-};
-
-export default Button;
